3733e080d897b4dd11f8e580ac35f8edcdc46db4
Jason Kulak <JasonKulak@yahoo.com>
2024-06-15 19:55:27 -0500

    practicting functions

--- scratch.js
+++ scratch.js
@@ -1,9 +1,24 @@
-const str = "abcde";
-const pairs = [];
+// const str = "abcde";
+// const pairs = [];
 
-for (let i = 0; i < str.length; i += 2) {
-    if(str[i+1]) pairs.push(str[i]+str[i+1])
-    else pairs.push(str[i]+'_')
+// for (let i = 0; i < str.length; i += 2) {
+//     if(str[i+1]) pairs.push(str[i]+str[i+1])
+//     else pairs.push(str[i]+'_')
+// }
+
+// console.log(pairs)
+
+// const createHelloWorld = function() {
+//     return function printHelloWorld(){
+//         return 'Hello World';
+//     }
+//     return printHelloWorld;
+// }
+
+// const myFunction = createHelloWorld();
+// console.log(myFunction)
+
+function greet(){
+    return "hello World";
 }
-
-console.log(pairs)
+console.log(greet())
